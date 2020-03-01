e155a0d28e5b322271b0a1b36c6292faca139077
hasewo <masato.sakai1103@gmail.com>
2020-03-01 18:17:53 +0900

    削除機能を追加

--- project/diary/urls.py
+++ project/diary/urls.py
@@ -6,5 +6,6 @@ app_name = 'diary'
 urlpatterns = [
     path('', views.index, name='index'), #/diary/index
 	path('add/', views.add, name='add'), #/diary/add
-	path('update/<int:pk>/', views.update, name='update')
+	path('update/<int:pk>/', views.update, name='update'), # /diary/update/1
+	path('delete/<int:pk>/', views.delete, name='delete'), # /diary/delete/1
 ]
