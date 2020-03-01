f70a8d53f563c50124002a94ab2a643bc2a0af3e
hasewo <masato.sakai1103@gmail.com>
2020-03-01 16:07:49 +0900

    見栄えを少し整備

--- project/diary/views.py
+++ project/diary/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from .forms import DayCreateForm
+from .models import Day
 
 def index(request):
-	return render(request, 'diary/day_list.html')
+	context = {
+		'day_list':Day.objects.all(),
+	}
+	return render(request, 'diary/day_list.html', context)
 
 
 def add(request):
@@ -12,10 +16,10 @@ def add(request):
 	# method=POST、つまり送信ボタン押下時入力内容が問題なければ
 	if request.method == 'POST' and form.is_valid():
 		form.save()
-		return redirect('diary.index')
+		return redirect('diary:index')
 
 	# 通常時のページアクセスや、入力内容に誤りがあればまたページを表示
 	context = {
-		'form': DayCreateForm()
+		'form': form
 	}
 	return render(request, 'diary/day_form.html', context)
